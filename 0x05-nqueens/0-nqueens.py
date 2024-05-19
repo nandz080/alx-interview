0c7d15083e0ddb769487a81b522ebfc222a5beed
nandz080 <nandz080@gmail.com>
2024-05-19 15:37:33 +0000

    [task0] implement a backtracking algorithm in Python

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,53 +3,50 @@
 """
 import sys
 
-def is_safe(board, row, column):
-    """Method that checks if it's safe to place a queen at board[row][col]"""
-    for i in range(row):
-        if board[i] == column or \
-           board[i] - i == column - row or \
-           board[i] + i == column + row:
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+
+try:
+    n_queens = int(sys.argv[1])
+except ValueError:
+    print('N must be a number')
+    exit(1)
+
+if n_queens < 4:
+    print('N must be at least 4')
+    exit(1)
+
+
+def solve_nQ(n):
+    """Method that solves the N queens problem"""
+    if n == 0:
+        return [[]]
+    first_solution = solve_nQ(n - 1)
+    return [solution + [(n, i + 1)]
+            for i in range(n_queens)
+            for solution in first_solution
+            if safeQueen((n, i + 1), solution)]
+
+
+def attackQueen(square, queen):
+    """Method for the queen to attack"""
+    (row, column) = square
+    (row1, column1) = queen
+    return (row == row1) or (column == column1) or\
+        abs(row - row1) == abs(column - column1)
+
+
+def safeQueen(sqr, queens):
+    """Method for the queen to be safe"""
+    for queen in queens:
+        if attackQueen(sqr, queen):
             return False
     return True
 
-def solve_nqueens(N):
-    """Method that solves the N Queens problem and prints all solutions"""
-    def solve(row, board):
-        if row == N:
-            print_board(board)
-            return
-        for column in range(N):
-            if is_safe(board, row, column):
-                board[row] = column
-                solve(row + 1, board)
-                board[row] = -1  # Backtrack
 
-    def print_board(board):
-        """Method that prints solution"""
-        solution = []
-        for i in range(N):
-            solution.append([i, board[i]])
-        print(solution)
-    
-    board = [-1] * N
-    solve(0, board)
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    
-    solve_nqueens(N)
-
-if __name__ == "__main__":
-    main()
+for answer in reversed(solve_nQ(n_queens)):
+    result = []
+    for p in [list(p) for p in answer]:
+        result.append([i - 1 for i in p])
+    print(result)
