e0710b08c4a1f8929feabcdff97d4094d1c864eb
nandz080 <nandz080@gmail.com>
2024-08-01 12:31:39 -0700

    [task 0] create a func def island_perimeter(grid) that returns the perimeter of the island described in grid

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in grid
+    Returns:
+    int: Perimeter of the island.
+    """
+    h_cells = len(grid)
+    v_cells = len(grid[0])
+    perimeter = 0
+    
+    for i in range(h_cells):
+        for j in range(v_cells):
+            if grid[i][j] == 1:
+                # Check all four possible sides (top, bottom, left, right)
+                if i == 0 or grid[i-1][j] == 0:  # Top side
+                    perimeter += 1
+                if i == h_cells-1 or grid[i+1][j] == 0:  # Bottom side
+                    perimeter += 1
+                if j == 0 or grid[i][j-1] == 0:  # Left side
+                    perimeter += 1
+                if j == v_cells-1 or grid[i][j+1] == 0:  # Right side
+                    perimeter += 1
+    return perimeter
